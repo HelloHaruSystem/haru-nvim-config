fe35c17d61534e922642524e6f94bdfda0905c5f
Halfdan Sand Christensen <halfdansc@proton.me>
2025-11-30 23:00:48 +0100

    Ex keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,19 @@
 -- Key remaps
--- space + space  + x to source
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
--- Space + x to executes the current line of lua code 
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
--- Space + x in visual mode executes the currently selected block of lua code
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+-- Leader key = space
+vim.g.mapleader = " "
+
+-- leader + pv for :Ex
+-- opens directory where the current fie is placed
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+
+-- TODO: add keymaps for Vex or Sex or both
+
+-- leader + leader + x to source
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+-- Leader + x to executes the current line of lua code 
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+-- leader + x in visual mode executes the currently selected block of lua code
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
 -- LSP Features Mappings
 -- Show Hover Documentation (K)
