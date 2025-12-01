522253f9c4702b0b76bc1219ec4fbf6fafa08924
Halfdan Sand Christensen <halfdansc@proton.me>
2025-12-01 14:43:30 +0100

    Scroll off set

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,15 +1,18 @@
 -- lua/config/settings.lua
 
--- Global 4 Space Indenting 
+-- Global 4 Space Indenting
 vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4        
-vim.opt.expandtab = true    
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
 
 -- Clipboard setting
 vim.opt.clipboard = "unnamedplus"
 
--- always fat cursor
+-- Always fat cursor
 vim.o.guicursor = ""
+
+-- Verticalscroll offset
+vim.opt.scrolloff = 10
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
