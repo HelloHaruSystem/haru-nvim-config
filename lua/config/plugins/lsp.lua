9c649dc42e41ea4fb08e5979b6baa55b575fb10c
Halfdan Sand Christensen <halfdansc@proton.me>
2025-12-01 11:57:40 +0100

    lsp formatting

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -4,39 +4,57 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       {
-	"folke/lazydev.nvim",
-	ft = "lua", -- only load on lua files
-	opts = {
-	  library = {
-	    -- See the configuration section for more details
-	    -- Load luvit types when the `vim.uv` word is found
-	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	  },
-	},
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
       },
     },
     config = function()
       -- global diagnostic setup (Done Once)
       vim.diagnostic.config({
-	virtual_text = {
-	  severity = { min = vim.diagnostic.severity.WARN }, 
-	  source = true, 
-	},
-	signs = true,
-	update_in_insert = false,
+        virtual_text = {
+          severity = { min = vim.diagnostic.severity.WARN }, 
+          source = true, 
+        },
+        signs = true,
+        update_in_insert = false,
       })
+
+      -- Formatting
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          if client.supports_method('textDocument/formatting', 0) then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end
+            })
+          end
+        end,
+        })
       -- global setup over
 
       -- specific lsp configs
       -- Lua Language Server (lua_ls)
       vim.lsp.config('lua_ls', {
-	settings = {
-	  Lua = {
-	    diagnostics = {
-	      globals = {'vim'}, 
-	    },
-	  },
-	},
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {'vim'}, 
+            },
+          },
+        },
       })
 
       -- Zig Language Server (zls)
