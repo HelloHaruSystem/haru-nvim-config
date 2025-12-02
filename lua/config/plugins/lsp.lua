4373797d40e66c549e742652b59ccff8c70aa5e5
Halfdan Sand Christensen <halfdansc@proton.me>
2025-12-02 15:17:51 +0100

    fixed zls formatting issue

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -20,6 +20,20 @@ return {
     }
   },
 
+  -- Mason Tool Installer (Formatters & Linters)
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua",   -- Lua Formatter
+          "prettier", -- JS/TS/JSON/YAML Formatter
+        },
+      })
+    end,
+  },
+
   -- Lsp config
   {
     "neovim/nvim-lspconfig",
@@ -46,25 +60,6 @@ return {
         signs = true,
         update_in_insert = false,
       })
-
-      -- Formatting on save
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
-
-          if client:supports_method('textDocument/formatting') then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end
-            })
-          end
-        end,
-      })
-      -- global setup over
 
       -- specific lsp configs
       -- Lua Language Server (lua_ls)
