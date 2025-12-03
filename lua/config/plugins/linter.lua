5232d7ffe575494de6fc69261861734d9740115e
Halfdan Sand Christensen <halfdansc@proton.me>
2025-12-03 10:18:43 +0100

    python linter

---
+++ lua/config/plugins/linter.lua
@@ -0,0 +1,28 @@
+-- Lua/config/plugins/linter.lua
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      -- Configure linters by filetype
+      lint.linters_by_ft = {
+        python = { "ruff" },
+        -- Potentially add support for JS/TS stuff
+      }
+
+      -- Create autocommand to trigger linting
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      -- Optional manual lint trigger keymap
+    end,
+  },
+}
