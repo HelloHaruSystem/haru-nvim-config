72a63e1f30452796340d674ae05323d6fc482238
Halfdan Sand Christensen <halfdansc@proton.me>
2025-12-29 11:33:43 +0100

    updated settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,5 +1,9 @@
 -- lua/config/settings.lua
 
+-- Line numbers (globally)
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
 -- Global 4 Space Indenting
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
@@ -25,3 +29,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
+
+-- Better editing
+vim.opt.smartindent = true
+vim.opt.signcolumn = "yes" -- Prevents jitter when LSP shows diagnostics
+vim.opt.updatetime = 250 -- Faster completion (default is 4000ms)
+vim.opt.incsearch = true -- Show search matches as you type
