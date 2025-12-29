d8b58d4aecc569c8c4b329e528c6b96cc9a985e3
Halfdan Sand Christensen <halfdansc@proton.me>
2025-12-29 11:19:22 +0100

    autotag plugin

---
+++ lua/config/plugins/autotag.lua
@@ -0,0 +1,15 @@
+-- lua/config/plugins/autotag.lua
+return {
+  "windwp/nvim-ts-autotag",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("nvim-ts-autotag").setup({
+      opts = {
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+      },
+    })
+  end,
+}
